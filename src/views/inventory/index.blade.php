cb715226d08189e0161667574cdebc50674d01c2
prabhapandeyy <133662676+prabhapandeyy@users.noreply.github.com>
2023-05-23 18:35:01 +0530

    Update index.blade.php

--- src/views/inventory/index.blade.php
+++ src/views/inventory/index.blade.php
@@ -1,20 +1,14 @@
-    <script src="{{ asset('/js/jquery.min.js') }}"></script>
-    <link href="{{ asset('/css/jquery.dataTables.min.css')}}" rel="stylesheet" type="text/css" />
-    <script src="{{ asset('/js/jquery.dataTables.min.js')}}"></script>
-        <script>
-               var table;
-               var lang;
-lang = "{{session()->get('applocale')??'en'}}";
-            table = $('#my-table').DataTable({
-                'columnDefs': [ {
-        'targets': [0,1,2,3,4,], /* column index */
-        'orderable': false, /* true or false */
-     }],
-     "language": {
-    "url": lang=='es' ? '/datatablelang/es-ES.json' : '',
-}
-            });
-            </script>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Inventory</title>
+  </head>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <body>
     <header class="title">
         <h1>{{ __('Inventory') }}</h1>
         <div><a href="{{ route('inventory.create') }}" class="btn btn-sm btn-primary">
@@ -30,7 +24,7 @@ lang = "{{session()->get('applocale')??'en'}}";
                 </div>
             @endif
             <div class="formwrap">
-                <table class="table table-hover" id="my-table">
+                <table class="table table-striped" id="my-table">
                     <thead>
                         <tr class=" table-primary">
                             <th scope="col">{{ __('Item Name') }}</th>
@@ -65,7 +59,8 @@ lang = "{{session()->get('applocale')??'en'}}";
             {{$inventory->render()}}
         </div>
     </div>
-
+</body>
+</html>
 
 
 
